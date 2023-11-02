07654cd693d0f51ffbabd64c2daf612262886947
tantality <angel.tretyak@mail.ru>
2023-11-02 16:17:11 +0300

    feat: implement bubble sort

---
+++ task-1/bubble-sort.js
@@ -0,0 +1,14 @@
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+
+  return arr;
+}
+
+const data = Array.from(Array(100000), () => Math.floor(Math.random() * 100) + 1);
+const sortedData = bubbleSort(data);
