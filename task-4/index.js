c43b6353f3f901187f603c06bcb086c508ec6f30
tantality <angel.tretyak@mail.ru>
2023-11-10 11:58:19 +0300

    feat: add side-to-side animation of a square using a timer

---
+++ task-4/index.js
@@ -0,0 +1,31 @@
+const PADDING_LEFT = 15;
+
+function animateSquareUsingSetTimeout(square) {
+  let leftPosition = PADDING_LEFT;
+  const squareWidth = square.getBoundingClientRect().width;
+  const roundedSquareWidth = Math.round(squareWidth);
+  let inverseDirection = false;
+
+  setInterval(frame, 5);
+
+  function frame() {
+    const parentWidth = square.parentNode.getBoundingClientRect().width;
+    const roundedParentWidth = Math.round(parentWidth);
+    const lastPossiblePosition =
+      roundedParentWidth - roundedSquareWidth - PADDING_LEFT;
+
+    if (leftPosition === lastPossiblePosition && !inverseDirection) {
+      inverseDirection = true;
+    }
+
+    if (leftPosition === PADDING_LEFT && inverseDirection) {
+      inverseDirection = false;
+    }
+
+    leftPosition = inverseDirection ? leftPosition - 1 : leftPosition + 1;
+    square.style.left = leftPosition + "px";
+  }
+}
+
+const square = document.querySelector("#square-for-set-timeout-animation");
+animateSquareUsingSetTimeout(square);
