5779334bcb4a2467d8719f98a235bf8d35b3ede9
tantality <angel.tretyak@mail.ru>
2023-11-10 13:13:15 +0300

    feat: add side-to-side animation of a square using requestAnimationFrame

--- task-4/index.js
+++ task-4/index.js
@@ -27,5 +27,39 @@ function animateSquareUsingSetTimeout(square) {
   }
 }
 
+function animateSquareUsingRequestAnimationFrame(square) {
+  let leftPosition = PADDING_LEFT;
+  const squareWidth = square.getBoundingClientRect().width;
+  const roundedSquareWidth = Math.round(squareWidth);
+  let inverseDirection = false;
+
+  requestAnimationFrame(frame);
+
+  function frame() {
+    const parentWidth = square.parentNode.getBoundingClientRect().width;
+    const roundedParentWidth = Math.round(parentWidth);
+    const lastPossiblePosition =
+      roundedParentWidth - roundedSquareWidth - PADDING_LEFT;
+
+    if (leftPosition === lastPossiblePosition && !inverseDirection) {
+      inverseDirection = true;
+    }
+
+    if (leftPosition === PADDING_LEFT && inverseDirection) {
+      inverseDirection = false;
+    }
+
+    leftPosition = inverseDirection ? leftPosition - 1 : leftPosition + 1;
+    square.style.left = leftPosition + "px";
+
+    requestAnimationFrame(frame);
+  }
+}
+
 const square = document.querySelector("#square-for-set-timeout-animation");
 animateSquareUsingSetTimeout(square);
+
+const square1 = document.querySelector(
+  "#square-for-request-animation-frame-animation"
+);
+animateSquareUsingRequestAnimationFrame(square1);
