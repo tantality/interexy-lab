e1ef318190f17596cdecb516c464cbacfd284038
tantality <angel.tretyak@mail.ru>
2023-11-13 11:04:07 +0300

    feat: split an array into chunks of data for sorting

--- task-4/sorts/bubble-sort-with-set-timeout.js
+++ task-4/sorts/bubble-sort-with-set-timeout.js
@@ -1,10 +1,12 @@
 const delay = async (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 async function bubbleSort(arr) {
+  const CHUNK_SIZE = 100;
   for (let i = 0; i < arr.length - 1; i++) {
+    if (i % CHUNK_SIZE === 0) await delay(0);
+
     for (let j = 0; j < arr.length - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
-        await delay(0);
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
       }
     }
@@ -13,7 +15,7 @@ async function bubbleSort(arr) {
   return arr;
 }
 
-const data = Array.from(Array(100), () => Math.floor(Math.random() * 100) + 1);
+const data = Array.from(Array(50000), () => Math.floor(Math.random() * 100) + 1);
 
 (async function () {
   console.time("Bubble sort execution time");
