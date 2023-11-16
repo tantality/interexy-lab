b555839d75da358d08c6fcec20fb88e21e4bf94c
tantality <angel.tretyak@mail.ru>
2023-11-16 12:15:53 +0300

    feat: typify a webpack config

---
+++ task-4/webpack.config.ts
@@ -0,0 +1,39 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import webpack from "webpack";
+
+interface EnvVariables {
+  mode: MODE;
+}
+
+enum MODE {
+  DEVELOPMENT = "development",
+  PRODUCTION = "production",
+}
+
+export default (env: EnvVariables) => {
+  const config: webpack.Configuration = {
+    mode: env.mode ?? MODE.DEVELOPMENT,
+    entry: path.resolve(__dirname, "src", "index.ts"),
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+    output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "[name].[contenthash].js",
+      clean: true,
+    },
+    plugins: [new HtmlWebpackPlugin({ template: path.resolve(__dirname, "src", "index.html") })],
+  };
+
+  return config;
+};
