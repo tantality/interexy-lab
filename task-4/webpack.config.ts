7a2957b690eb3a5909586942bb20606185d1c37b
tantality <angel.tretyak@mail.ru>
2023-11-16 13:45:40 +0300

    feat: config the webpack-dev-server

--- task-4/webpack.config.ts
+++ task-4/webpack.config.ts
@@ -1,9 +1,11 @@
 import path from "path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import webpack from "webpack";
+import { Configuration as DevServerConfiguration } from "webpack-dev-server";
 
 interface EnvVariables {
   mode: MODE;
+  port: number;
 }
 
 enum MODE {
@@ -11,7 +13,15 @@ enum MODE {
   PRODUCTION = "production",
 }
 
+const DEFAULT_VALUE = {
+  PORT: 3000,
+  MODE: MODE.DEVELOPMENT,
+};
+
 export default (env: EnvVariables) => {
+  const isDevMode = env.mode === MODE.DEVELOPMENT;
+  const devServer: DevServerConfiguration = { port: env.port ?? DEFAULT_VALUE.PORT, open: true };
+
   const config: webpack.Configuration = {
     mode: env.mode ?? MODE.DEVELOPMENT,
     entry: path.resolve(__dirname, "src", "index.ts"),
@@ -33,6 +43,8 @@ export default (env: EnvVariables) => {
       clean: true,
     },
     plugins: [new HtmlWebpackPlugin({ template: path.resolve(__dirname, "src", "index.html") })],
+    devtool: isDevMode && "inline-source-map",
+    devServer: isDevMode ? devServer : undefined,
   };
 
   return config;
