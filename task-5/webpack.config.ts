10f4f823317d2ce5516eaf7938da7bcb1653a2c7
tantality <angel.tretyak@mail.ru>
2023-11-20 11:09:00 +0300

    feat: adjust webpack and typescript configurations

---
+++ task-5/webpack.config.ts
@@ -0,0 +1,36 @@
+import path from "path";
+import webpack from "webpack";
+
+interface EnvVariables {
+  mode: BUILD_MODE;
+}
+
+enum BUILD_MODE {
+  DEVELOPMENT = "development",
+  PRODUCTION = "production",
+}
+
+export default (env: EnvVariables) => {
+  const cwd = process.cwd();
+
+  const config: webpack.Configuration = {
+    mode: env.mode ?? BUILD_MODE.DEVELOPMENT,
+    entry: path.resolve(__dirname, "src", "index.ts"),
+    module: {
+      rules: [{ test: /\.tsx?$/, use: "ts-loader", exclude: /node_modules/ }],
+    },
+    resolve: {
+      alias: {
+        src: path.resolve(cwd, "src/"),
+      },
+      extensions: [".ts", ".js"],
+    },
+    output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "[name].[contenthash].js",
+      clean: true,
+    },
+  };
+
+  return config;
+};
