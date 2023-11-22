6683469ed02422a79dbdc10568dd2a0c5ba7a74a
tantality <angel.tretyak@mail.ru>
2023-11-22 17:39:02 +0300

    feat: add a method for getting a character

--- task-6/src/app/providers/rick-and-morty.client.ts
+++ task-6/src/app/providers/rick-and-morty.client.ts
@@ -1,9 +1,15 @@
 import { axiosInstance } from "./rick-and-morty.axios-instance";
 import { GetAllCharactersWithPageInfoParams, CharactersWithPaginationData } from "./types/all-characters.types";
+import { Character } from "./types/character.types";
 
 class RickAndMortyApi {
   async getAllCharacters(params: GetAllCharactersWithPageInfoParams = {}): Promise<CharactersWithPaginationData> {
     const res = await axiosInstance.get<CharactersWithPaginationData>(`character/`, { params });
+    return res.data;
+  }
+
+  async getCharacter(id: number): Promise<Character> {
+    const res = await axiosInstance.get<Character>(`character/${id}`);
     return res.data;
   }
 }
