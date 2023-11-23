e6d009d65c7aa7769868868f3054dd0e897a2eb5
tantality <angel.tretyak@mail.ru>
2023-11-23 11:49:13 +0300

    feat: wrap the character cards with links

--- task-6/src/app/characters/components/character-list/character-list.component.tsx
+++ task-6/src/app/characters/components/character-list/character-list.component.tsx
@@ -1,14 +1,18 @@
 import { Grid } from "@mantine/core";
 import React from "react";
+import { Link } from "react-router-dom";
 import { Character } from "../../../providers/types/character.types";
 import CharacterCard from "../character-card/character-card.component";
 
 class CharacterList extends React.Component<{ characters: Character[] }> {
   render() {
-    const characterCards = this.props.characters.map((data) => {
+    const { characters } = this.props;
+    const characterCards = characters.map((character) => {
       return (
-        <Grid.Col style={{ maxWidth: '350px' }} key={data.id}>
-          <CharacterCard {...data} />
+        <Grid.Col style={{ maxWidth: '350px' }} key={character.id}>
+          <Link to={`/characters/${character.id}`}>
+            <CharacterCard {...character} imageHeight={260} />
+          </Link>
         </Grid.Col>
       )
     });
