7a3f698de510d1d9c67d309d0a497364b53fdfbe
tantality <angel.tretyak@mail.ru>
2023-11-26 18:02:30 +0300

    feat: implement the character card

--- task-7/src/app/characters/components/character-card.comp.tsx
+++ task-7/src/app/characters/components/character-card.comp.tsx
@@ -1,4 +1,5 @@
-import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+import { Box, Card, CardContent, CardMedia, Stack, Typography } from "@mui/material";
+import HighlightedTextFragment from "components/highlighted-text-fragment.comp";
 import { Character } from "providers/types/character.types";
 import { FC } from "react";
 
@@ -9,16 +10,46 @@ interface CharacterCardProps {
 }
 
 const CharacterCard: FC<CharacterCardProps> = ({ character, cardMaxWidth, imgHeight }) => {
-  const { image, name } = character;
+  const { image, name, status, species, gender, location, origin } = character;
+
+  const descriptionTextProps = {
+    fontSize: "1.2rem"
+  };
 
   return (
-    <Card sx={{ maxWidth: cardMaxWidth ?? null, minWidth: '300px' }}>
-      <CardMedia image={image} title={name} sx={{ height: imgHeight ?? '350px' }} />
+    <Card sx={{ maxWidth: cardMaxWidth ?? null, minWidth: "300px" }}>
+      <CardMedia image={image} title={name} sx={{ height: imgHeight ?? "300px" }} />
       <CardContent>
-        <Typography variant="h3">{name}</Typography>
+        <Stack rowGap="8px">
+          <Typography variant="h3" sx={{ fontSize: "1.5rem", fontWeight: 600 }}>
+            {name}
+          </Typography>
+          <Box>
+            <Typography sx={descriptionTextProps}>
+              <HighlightedTextFragment>Status:</HighlightedTextFragment>
+              {status}
+            </Typography>
+            <Typography sx={descriptionTextProps}>
+              <HighlightedTextFragment>Species:</HighlightedTextFragment>
+              {species}
+            </Typography>
+            <Typography sx={descriptionTextProps}>
+              <HighlightedTextFragment>Gender:</HighlightedTextFragment>
+              {gender}
+            </Typography>
+            <Typography sx={descriptionTextProps}>
+              <HighlightedTextFragment>Origin location:</HighlightedTextFragment>
+              {location.name}
+            </Typography>
+            <Typography sx={descriptionTextProps}>
+              <HighlightedTextFragment>Current location:</HighlightedTextFragment>
+              {origin.name}
+            </Typography>
+          </Box>
+        </Stack>
       </CardContent>
-    </Card >
+    </Card>
   );
-}
+};
 
 export default CharacterCard;
