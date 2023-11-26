7a96428d95c9457fbc5876f09dcbc58c42c5df33
tantality <angel.tretyak@mail.ru>
2023-11-26 21:06:47 +0300

    feat: transfer code to a smart component

--- task-7/src/app/characters/components/character-view-conent.comp.tsx
+++ task-7/src/app/characters/components/character-view-conent.comp.tsx
@@ -1,21 +1,16 @@
 import { Button, Stack } from "@mui/material";
-import { useAsync } from "hooks";
-import rickAndMortyClient from "providers/rick-and-morty.client";
-import { Character } from "providers/types/character.types";
 import { FC } from "react";
 import { useParams } from "react-router-dom";
-import CharacterCard from "./character-card.comp";
+import SmartCharacterCard from "./smart-character-card.comp";
 
 const CharacterViewContent: FC = () => {
   const { characterId: characterIdParam } = useParams();
   const characterId = Number(characterIdParam);
 
-  const { data: character } = useAsync<Character>(() => rickAndMortyClient.getCharacter(characterId), [characterId]);
-
   return (
     <section className="character-view-content">
       <Stack rowGap="50px" alignItems="center">
-        {character && <CharacterCard character={character} imgHeight="450px" cardMaxWidth="450px" key={characterId} />}
+        <SmartCharacterCard key={characterId} characterId={characterId} />
         <Button href="/characters" variant="outlined" size="large" sx={{ border: "1px solid black", color: "black" }}>
           All characters
         </Button>
---
+++ task-7/src/app/characters/components/smart-character-card.comp.tsx
@@ -0,0 +1,21 @@
+import { useAsync } from "hooks";
+import rickAndMortyClient from "providers/rick-and-morty.client";
+import { Character } from "providers/types/character.types";
+import { FC } from "react";
+import CharacterCard from "./character-card.comp";
+
+interface SmartCharacterCardProps {
+  characterId: number;
+}
+
+const SmartCharacterCard: FC<SmartCharacterCardProps> = ({ characterId }) => {
+  const { data: character } = useAsync<Character>(() => rickAndMortyClient.getCharacter(characterId), [characterId]);
+
+  return (
+    <>
+      {character && <CharacterCard character={character} imgHeight="450px" cardMaxWidth="450px" key={characterId} />}
+    </>
+  );
+};
+
+export default SmartCharacterCard;
