b73cbb13af81c4e88dfb2b9fcfebc2114a42aeb5
tantality <angel.tretyak@mail.ru>
2023-11-26 19:01:58 +0300

    feat: add a padding prop to the page-layout component

--- task-7/src/components/page-layout.comp.tsx
+++ task-7/src/components/page-layout.comp.tsx
@@ -3,10 +3,11 @@ import { Container } from '@mui/material';
 
 interface PageLayoutProps {
   children: ReactNode;
+  padding?: string;
 }
 
-const PageLayout: FC<PageLayoutProps> = ({ children }) => {
-  return <Container maxWidth="lg">{children}</Container>;
+const PageLayout: FC<PageLayoutProps> = ({ children, padding }) => {
+  return <Container maxWidth="lg" sx={{ padding: padding ?? '30px 0' }}>{children}</Container>;
 }
 
 
