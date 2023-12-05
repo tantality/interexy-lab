64ce2fbf4cb192ad32e301fad50ece8998ac3ea9
tantality <angel.tretyak@mail.ru>
2023-12-05 18:45:00 +0300

    feat: add post-dto

---
+++ task-8/src/domain/dto/post.dto.ts
@@ -0,0 +1,36 @@
+import { Post, User } from '@prisma/client';
+import { UUIDDto } from './uuid.dto';
+
+export class PostDto extends UUIDDto {
+  id: string;
+  title: string;
+  content: string;
+  created_at: number;
+  updated_at: number;
+
+  author?: User;
+
+  static fromEntity(entity?: Post & { author?: User }) {
+    if (!entity) {
+      return;
+    }
+
+    const it = new PostDto();
+    it.id = entity.id;
+    it.title = entity.title;
+    it.content = entity.content;
+    it.author = entity.author;
+    it.created_at = entity.created_at.valueOf();
+    it.updated_at = entity.updated_at.valueOf();
+
+    return it;
+  }
+
+  static fromEntities(entities?: Post[]) {
+    if (!entities?.map) {
+      return;
+    }
+
+    return entities.map((entity) => this.fromEntity(entity));
+  }
+}
