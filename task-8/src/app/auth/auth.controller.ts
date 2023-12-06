943f787b1b03c38b3b65e9d99f7157c622107f27
tantality <angel.tretyak@mail.ru>
2023-12-07 00:31:35 +0300

    feat: implement user registration

--- task-8/src/app/auth/auth.controller.ts
+++ task-8/src/app/auth/auth.controller.ts
@@ -1,7 +1,35 @@
-import { Controller } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  InternalServerErrorException,
+  Post,
+} from '@nestjs/common';
+import normalizeEmail from 'normalize-email';
 import { AuthService } from './auth.service';
+import { SignUpForm } from './domain/signup.form';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  async signup(body: SignUpForm) {
+    const normEmail = normalizeEmail(body.email);
+    const user = await this.authService.findUserByNormalizedEmail(normEmail);
+
+    if (user) {
+      throw new BadRequestException('User already exists');
+    }
+
+    const registeredUser = await this.authService.makeNewUser({
+      ...body,
+      normalized_email: normEmail,
+    });
+
+    if (!registeredUser) {
+      throw new InternalServerErrorException(`User hasn't been created`);
+    }
+
+    return registeredUser;
+  }
 }
