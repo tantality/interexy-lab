7be4694483f9ff2fa1d950d6b99afecc978ec2b9
tantality <angel.tretyak@mail.ru>
2023-12-07 01:26:30 +0300

    feat: import a module

--- task-8/src/app.module.ts
+++ task-8/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from 'app/auth/auth.module';
 import { PostsModule } from 'app/posts/posts.module';
 import { RolesModule } from 'app/roles/roles.module';
 import { UsersModule } from 'app/users/users.module';
 
 @Module({
-  imports: [PostsModule, RolesModule, UsersModule],
+  imports: [PostsModule, RolesModule, UsersModule, AuthModule],
 })
 export class AppModule {}
